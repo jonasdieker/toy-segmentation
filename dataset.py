85f3c0dc2f5faa75d45834beed6e8c6ad56ae8c1
Jonas Dieker <jonas.dieker@teraki.com>
2023-12-08 14:58:12 +0100

    working segmentation training

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torchvision.transforms as T
 from torch.utils.data import Dataset
@@ -5,30 +6,89 @@ from PIL import Image
 from glob import glob
 
 
+RGBLabel2LabelName = {
+    (128, 128, 128): "Sky",
+    (0, 128, 64): "Building",
+    (128, 0, 0): "Building",
+    (64, 192, 0): "Building",
+    (64, 0, 64): "Building",
+    (192, 0, 128): "Building",
+    (192, 192, 128): "Pole",
+    (0, 0, 64): "Pole",
+    (128, 64, 128): "Road",
+    (128, 0, 192): "Road",
+    (192, 0, 64): "Road",
+    (0, 0, 192): "Sidewalk",
+    (64, 192, 128): "Sidewalk",
+    (128, 128, 192): "Sidewalk",
+    (128, 128, 0): "Tree",
+    (192, 192, 0): "Tree",
+    (192, 128, 128): "SignSymbol",
+    (128, 128, 64): "SignSymbol",
+    (0, 64, 64): "SignSymbol",
+    (64, 64, 128): "Fence",
+    (64, 0, 128): "Car",
+    (64, 128, 192): "Car",
+    (192, 128, 192): "Car",
+    (192, 64, 128): "Car",
+    (128, 64, 64): "Car",
+    (64, 64, 0): "Pedestrian",
+    (192, 128, 64): "Pedestrian",
+    (64, 0, 192): "Pedestrian",
+    (64, 128, 64): "Pedestrian",
+    (0, 128, 192): "Bicyclist",
+    (192, 0, 192): "Bicyclist",
+    (0, 0, 0): "Void",
+}
+
+LabelName2LabelIndex = {
+    "Sky": 0,
+    "Building": 1,
+    "Pole": 2,
+    "Road": 3,
+    "Sidewalk": 4,
+    "Tree": 5,
+    "SignSymbol": 6,
+    "Fence": 7,
+    "Car": 8,
+    "Pedestrian": 9,
+    "Bicyclist": 10,
+    "Void": 11,
+}
+
+
 class CustomDataset(Dataset):
-    def __init__(self, data_root: str):
+    def __init__(self, image_root: str, mask_root):
         super().__init__()
 
-        self.image_paths = sorted(glob(f"{data_root}/images/*.png"))
-        self.gt_paths = sorted(glob(f"{data_root}/masks/*.png"))
+        self.image_paths = sorted(glob(f"{image_root}/*.png"))
+        self.gt_paths = sorted(glob(f"{mask_root}/*.png"))
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, index):
         # load data
-        image = Image.open(self.image_paths[index]).convert("RGB")
-        gt = Image.open(self.gt_paths[index]).convert("RGB")
+        image = np.array(Image.open(self.image_paths[index]).convert("RGB"))
+        gt_mask = np.array(Image.open(self.gt_paths[index]))
+        gt_mask = gt_mask / (0.039 * 25)
 
         # transform data
-        transforms = T.Compose(
+        transforms_img = T.Compose(
             [
+                T.ToTensor(),
                 T.RandomCrop(224),
                 T.RandomHorizontalFlip(p=0.3),
                 T.ConvertImageDtype(torch.float),
                 T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-                T.ToTensor(),
             ]
         )
-        image = transforms(image)
-        return (image, gt)
+        transforms_gt = T.Compose(
+            [
+                T.ToTensor(),
+                T.RandomCrop(224),
+                T.RandomHorizontalFlip(p=0.3),
+            ]
+        )
+        image, gt_mask = transforms_img(image), transforms_gt(gt_mask)
+        return image, gt_mask.squeeze().long()
--- train.py
+++ train.py
@@ -5,8 +5,11 @@ train func
 val func
 hyper-params
 """
+from tqdm import tqdm
 import torch
+import torch.nn as nn
 import torch.optim as optim
+from torch.optim import Optimizer
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 
@@ -15,3 +18,76 @@ from model import UNet
 
 # perform argmax over channels of output
 # apply crossentropy
+
+
+def train_val_epoch(
+    model: nn.Module,
+    dataloader: DataLoader,
+    loss_fn: CrossEntropyLoss,
+    epoch: int,
+    optimizer: Optimizer = None,
+    train: bool = True,
+):
+    running_loss = 0
+    pbar = tqdm(dataloader)
+    for i, data in enumerate(pbar):
+        pbar.set_description(f"[Epoch {str(epoch+1).zfill(3)}]: ")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        images, gts = data
+        images = images.to(device).double()
+        gts.to(device)
+
+        # zero gradients
+        if train:
+            optimizer.zero_grad()
+
+        # pass data through model
+        preds = model(images)
+        loss = loss_fn(preds, gts)
+        loss.backward()
+
+        # adjust weights
+        if train:
+            optimizer.step()
+
+        # update progress bar
+        running_loss += loss.item()
+        if i % 5 == 4:
+            last_loss = running_loss / 5
+            pbar.postfix(f"loss: {last_loss}")
+            running_loss = 0
+
+
+def main():
+    batch_size = 8
+    max_epochs = 20
+    lr = 0.0001
+    betas = (0.9, 0.999)
+    num_classes = 12
+
+    loss = CrossEntropyLoss()
+    model = UNet(3, num_classes).double()
+    optimizer = optim.Adam(model.parameters(), lr=lr, betas=betas)
+    train_dataset = CustomDataset(
+        image_root="/home/jonas/Downloads/CamVid/train",
+        mask_root="/home/jonas/Downloads/CamVid/trainannot",
+    )
+    val_dataset = CustomDataset(
+        image_root="/home/jonas/Documents/data/CamVid/val",
+        mask_root="/home/jonas/Documents/data/CamVid/valannot",
+    )
+
+    train_dataloader = DataLoader(
+        train_dataset, batch_size, shuffle=True, num_workers=4
+    )
+    val_dataloader = DataLoader(val_dataset, batch_size, shuffle=False, num_workers=4)
+
+    for epoch in range(max_epochs):
+        train_val_epoch(model, train_dataloader, loss, epoch, optimizer, train=True)
+        train_val_epoch(model, val_dataloader, loss, epoch, train=False)
+
+    torch.save(model.state_dict(), "model.pth")
+
+
+if __name__ == "__main__":
+    main()
