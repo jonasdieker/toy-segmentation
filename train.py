d26f8001f6b0aa1c337d9b84ca4380755b64c79d
Jonas Dieker <jonasdieker05@gmail.com>
2023-12-08 10:11:56 +0100

    add train imports

--- train.py
+++ train.py
@@ -5,3 +5,13 @@ train func
 val func
 hyper-params
 """
+import torch
+import torch.optim as optim
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+from dataset import CustomDataset
+from model import UNet
+
+# perform argmax over channels of output
+# apply crossentropy
