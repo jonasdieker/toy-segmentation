a48713fcd6812bf7e70508b42f6990cfa839bd5b
Jonas Dieker <jonasdieker05@gmail.com>
2023-12-09 15:59:12 +0100

    fix bug in dataset: now img and mask use same transformations

--- dataset.py
+++ dataset.py
@@ -58,8 +58,9 @@ LabelName2LabelIndex = {
 
 
 class CustomDataset(Dataset):
-    def __init__(self, image_root: str, mask_root):
+    def __init__(self, image_root: str, mask_root, test: bool=False):
         super().__init__()
+        self.test = test
 
         self.image_paths = sorted(glob(f"{image_root}/*.png"))
         self.gt_paths = sorted(glob(f"{mask_root}/*.png"))
@@ -69,32 +70,40 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, index):
         # load data
-        image = np.array(Image.open(self.image_paths[index]).convert("RGB"))
+        image = np.array(Image.open(self.image_paths[index]))
         gt = np.array(Image.open(self.gt_paths[index]))
-        gt = gt / (0.039 * 25)
 
         # map rgb value to class idx
         gt_mask = np.zeros((gt.shape[0], gt.shape[1]))
         for key, val in RGBLabel2LabelName.items():
-          label_idx = LabelName2LabelIndex[val]
-          gt_mask[np.all(gt == key, axis=-1)] = label_idx
+            label_idx = LabelName2LabelIndex[val]
+            gt_mask[np.all(gt == key, axis=-1)] = label_idx
 
-        # transform data
-        transforms_img = T.Compose(
-            [
-                T.ToTensor(),
-                T.RandomCrop(224),
-                T.RandomHorizontalFlip(p=0.3),
-                T.ConvertImageDtype(torch.float),
-                T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            ]
-        )
-        transforms_gt = T.Compose(
-            [
-                T.ToTensor(),
-                T.RandomCrop(224),
-                T.RandomHorizontalFlip(p=0.3),
-            ]
-        )
-        image, gt_mask = transforms_img(image), transforms_gt(gt_mask)
+        if not self.test:
+            # concat both to apply same transformations
+            transform2torch = T.ToTensor()
+            gt_mask = transform2torch(gt_mask)
+            gt_mask = gt_mask.expand(3, gt_mask.shape[1], gt_mask.shape[2])
+            concat_data = torch.cat((transform2torch(image).unsqueeze(0), gt_mask.unsqueeze(0)), 0)
+
+            # transform data
+            transform = T.Compose(
+                [
+                    T.RandomCrop(640),
+                    T.RandomHorizontalFlip(p=0.3),
+                ]
+            )
+            transform_img = T.Compose(
+                [
+                    T.ConvertImageDtype(torch.float),
+                    # T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                ]
+            )
+            concat_data = transform(concat_data)
+            image, gt_mask = concat_data[0], concat_data[1, 0]
+            image = transform_img(image)
+        else:
+            transform = T.ToTensor()
+            image, gt_mask = transform(image), transform(gt_mask)
+
         return image, gt_mask.squeeze().long()
