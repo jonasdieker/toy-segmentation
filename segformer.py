fcbdfc4924ba17827f1c268376daa5a581b23779
Jonas Dieker <jonasdieker05@gmail.com>
2023-12-10 17:13:43 +0100

    working smallest segformer model

--- segformer.py
+++ segformer.py
@@ -1,3 +1,4 @@
+import warnings
 from functools import partial
 
 import torch
@@ -216,6 +217,8 @@ class MixVisionTransformer(nn.Module):
 
 
 class mit_b0(MixVisionTransformer):
+    """Smallest SegFormer backbone with ~3.4 million parameters."""
+
     def __init__(self, **kwargs):
         super(mit_b0, self).__init__(
             patch_size=4, embed_dims=[32, 64, 160, 256], num_heads=[1, 2, 5, 8], mlp_ratios=[4, 4, 4, 4],
@@ -223,10 +226,118 @@ class mit_b0(MixVisionTransformer):
             drop_rate=0.0)
         
 
+class MLP(nn.Module):
+    """
+    Linear Embedding
+    """
+    def __init__(self, input_dim=2048, embed_dim=768):
+        super().__init__()
+        self.proj = nn.Linear(input_dim, embed_dim)
+
+    def forward(self, x):
+        x = x.flatten(2).transpose(1, 2)
+        x = self.proj(x)
+        return x
+    
+
+def resize(input,
+           size=None,
+           scale_factor=None,
+           mode='nearest',
+           align_corners=None,
+           warning=True):
+    if warning:
+        if size is not None and align_corners:
+            input_h, input_w = tuple(int(x) for x in input.shape[2:])
+            output_h, output_w = tuple(int(x) for x in size)
+            if output_h > input_h or output_w > output_h:
+                if ((output_h > 1 and output_w > 1 and input_h > 1
+                     and input_w > 1) and (output_h - 1) % (input_h - 1)
+                        and (output_w - 1) % (input_w - 1)):
+                    warnings.warn(
+                        f'When align_corners={align_corners}, '
+                        'the output would more aligned if '
+                        f'input size {(input_h, input_w)} is `x+1` and '
+                        f'out size {(output_h, output_w)} is `nx+1`')
+    if isinstance(size, torch.Size):
+        size = tuple(int(x) for x in size)
+    return F.interpolate(input, size, scale_factor, mode, align_corners)
+
+
+class SegFormerHead(nn.Module):
+    def __init__(self, in_channels, in_index, feature_strides, *, num_classes, decoder_params, dropout_ratio):
+        super().__init__()
+
+        assert len(feature_strides) == len(in_channels)
+        assert min(feature_strides) == feature_strides[0]
+        self.feature_strides = feature_strides
+        self.in_index = in_index
+
+        c1_in_channels, c2_in_channels, c3_in_channels, c4_in_channels = in_channels
+
+        embedding_dim = decoder_params['embed_dim']
+
+        self.linear_c4 = MLP(input_dim=c4_in_channels, embed_dim=embedding_dim)
+        self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
+        self.linear_c2 = MLP(input_dim=c2_in_channels, embed_dim=embedding_dim)
+        self.linear_c1 = MLP(input_dim=c1_in_channels, embed_dim=embedding_dim)
+
+        self.linear_fuse = nn.Sequential(
+            nn.Conv2d(embedding_dim * 4, embedding_dim, kernel_size=1),
+        )
+
+        self.linear_pred = nn.Conv2d(embedding_dim, num_classes, kernel_size=1)
+        self.dropout = nn.Dropout2d(dropout_ratio)
+
+
+    def forward(self, x):
+        c1, c2, c3, c4 = x
+
+        ############## MLP decoder on C1-C4 ###########
+        n, _, h, w = c4.shape
+
+        _c4 = self.linear_c4(c4).permute(0,2,1).reshape(n, -1, c4.shape[2], c4.shape[3])
+        _c4 = resize(_c4, size=c1.size()[2:],mode='bilinear',align_corners=False)
+
+        _c3 = self.linear_c3(c3).permute(0,2,1).reshape(n, -1, c3.shape[2], c3.shape[3])
+        _c3 = resize(_c3, size=c1.size()[2:],mode='bilinear',align_corners=False)
+
+        _c2 = self.linear_c2(c2).permute(0,2,1).reshape(n, -1, c2.shape[2], c2.shape[3])
+        _c2 = resize(_c2, size=c1.size()[2:],mode='bilinear',align_corners=False)
+
+        _c1 = self.linear_c1(c1).permute(0,2,1).reshape(n, -1, c1.shape[2], c1.shape[3])
+
+        _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1], dim=1))
+        _c = F.layer_norm(_c, _c.shape)
+
+        x = self.dropout(_c)
+        x = self.linear_pred(x)
+
+        return x
+
+
+class SegFormerHead_cfg(SegFormerHead):
+    def __init__(self, **kwargs):
+        super().__init__(
+            in_channels=[32, 64, 160, 256], in_index=[0, 1, 2, 3], feature_strides=[4, 8, 16, 32],
+            num_classes=12, decoder_params=dict(embed_dim=256), dropout_ratio=0.1
+        )  
+
+
+class SegFormer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bb = mit_b0()
+        self.head = SegFormerHead_cfg()
+
+    def forward(self, x):
+        x = self.bb(x)
+        x = self.head(x)
+        return x
+
+
 if __name__ == "__main__":
-    bb = mit_b0()
+    segformer = SegFormer()
     img = torch.randn((1, 3, 224, 224))
-    features = bb(img)
-    # outputs features at 4 different scales
-    for feature in features:
-        print(feature.shape)
+    out = segformer(img)
+    print(out.shape)
